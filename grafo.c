011f3c362fbc8f06446695fc852276b37453a7bd
Paolo Rechia <paolorechia@gmail.com>
2018-04-19 21:03:15 -0300

    Arquivos professor; dot exemplo do pdf; include header cgraph

---
+++ grafo.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "grafo.h"
+#include <graphviz/cgraph.h>
+
+//------------------------------------------------------------------------------
+// (apontador para) estrutura de dados para representar um grafo
+// 
+// o grafo pode ser direcionado ou não
+// 
+// o grafo tem um nome, que é uma "string"
+
+struct grafo {};
+
+//------------------------------------------------------------------------------
+// desaloca toda a memória usada em *g
+// 
+// devolve 1 em caso de sucesso,
+//         ou 
+//         0, caso contrário
+
+int destroi_grafo(grafo g) {
+  
+  return 0;
+}
+//------------------------------------------------------------------------------
+// lê um grafo no formato dot de input
+// 
+// devolve o grafo lido,
+//         ou 
+//         NULL, em caso de erro 
+
+grafo le_grafo(FILE *input) {
+    
+  
+  return 0;
+}
+//------------------------------------------------------------------------------
+// escreve o grafo g em output usando o formato dot.
+//
+// devolve o grafo escrito,
+//         ou 
+//         NULL, em caso de erro 
+
+grafo escreve_grafo(FILE *output, grafo g) {
+  
+  return g;
+}
+//------------------------------------------------------------------------------
+// devolve o grafo de recomendações de g
+//
+// cada aresta {c,p} de H representa uma recomendação do produto p
+// para o consumidor c, e tem um atributo "weight" que é um inteiro
+// representando a intensidade da recomendação do produto p para o
+// consumidor c.
+//
+// cada vértice de g tem um atributo "tipo" cujo valor é 'c' ou 'p',
+// conforme o vértice seja consumidor ou produto, respectivamente
+
+grafo recomendacoes(grafo g){
+
+  return g;
+}
+
+//------------------------------------------------------------------------------
---
+++ teste.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <graphviz/cgraph.h>
+#include "grafo.h"
+
+//------------------------------------------------------------------------------
+
+int main(void) {
+
+  grafo g = le_grafo(stdin);
+
+  if ( !g )
+
+    return 1;
+
+  escreve_grafo(stdout, g);
+
+  return ! destroi_grafo(g);
+}
