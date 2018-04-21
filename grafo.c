d5f4e3b0cbff0e178af96ec5e3a3fa2b70d9d01f
Paolo Rechia <paolorechia@gmail.com>
2018-04-21 04:04:31 -0300

    Ihul, seg fault na funcao de gerar uma lista da vizinhanca de um vertice; tamanho da lista; interseccao

--- grafo.c
+++ grafo.c
@@ -5,6 +5,16 @@
 #include "grafo.h"
 
 /* Estrutura de dados auxiliar - lista encadeada */
+/* Adaptar codigo da lista
+
+Incluir:
+
+1 - metodo de contagem do tamanho da lista (testar)
+2 - metodo de interseccao de duas listas (arrumar)
+3 - metodo de diferenca de duas listas
+4 - desalocar (free) na lista inteira
+
+*/
 typedef struct cel_struct{
     char chave[50];
     struct cel_struct * prox;
@@ -66,6 +76,36 @@ void busca_lista(tnodo * nodo, int buscado){
     return;
 }
 
+
+tnodo interseccao(tnodo *lista1, tnodo *lista2){
+    printf("b");
+    tnodo lista_inter;
+    tnodo * aux1;
+    tnodo * aux2;
+
+    aux1 = lista1;
+    aux2 = lista2;
+
+    printf("a");
+    inicia_lista(&lista_inter, "");
+    printf("a");
+    while (aux1->prox != NULL){
+        while (aux2->prox != NULL){
+            printf("b");
+            if (strcmp(aux1->chave, aux2->chave)){
+                insere_lista(&lista_inter, aux1->chave);
+            }
+            if (aux2->prox != NULL){
+                aux2 = aux2->prox;
+            }
+        } 
+        if (aux1->prox != NULL){
+            aux1 = aux1->prox;
+        }
+    }
+    return lista_inter;
+}
+
 void remocao_lista(tnodo *nodo, int buscado, tnodo *ant){
     if (nodo->chave != buscado)
         if (nodo->prox != NULL)
@@ -86,10 +126,15 @@ void remocao_lista(tnodo *nodo, int buscado, tnodo *ant){
     return;
 
 }
-    
+/* valor eh retornado em k */
+void tamanho_lista(tnodo * nodo, int * k){
+    if (nodo->prox!=NULL){
+        *k++;
+        tamanho_lista(nodo->prox, k);
+    }
+}
 
 
-#define DEBUG 1
 //------------------------------------------------------------------------------
 // (apontador para) estrutura de dados para representar um grafo
 // 
@@ -158,7 +203,8 @@ int eh_consumidor(Agnode_t *n, Agsym_t* sym){
 // guardar essa funcao para depois
 
 
-tnodo visita_vizinhanca(Agraph_t *g, Agnode_t *n){
+/* Consertar seg fault */
+tnodo * visita_vizinhanca(Agraph_t *g, Agnode_t *n){
             Agedge_t *e;
             tnodo lista;
             inicia_lista(&lista, "");
@@ -173,27 +219,36 @@ tnodo visita_vizinhanca(Agraph_t *g, Agnode_t *n){
                     }
             }
             imprime_lista(&lista);
-            return lista;
+                    printf("BREAK:\n");
+            return &lista;
 }
 
+// implementar:
+// interseccao(lista1, lista2)
+// diferenca(lista1, lista) 
 
 
 grafo recomendacoes(grafo g){
     Agnode_t *v;
     Agnode_t *u;
     Agedge_t *e;
+    tnodo * lista1, *lista2;
 //    char str[100];
     Agsym_t* sym = agattr(g,AGNODE,"tipo", 0);
     for (v = agfstnode(g); v; v = agnxtnode(g,v)) {
         if (eh_consumidor(v, sym)){
             
             printf("Vértice: %s (tipo:%s)\n",agnameof(v), agxget(v,sym));
-            visita_vizinhanca(g, v);
+            lista1 = visita_vizinhanca(g, v);
             u = agnxtnode(g, v);
             while (u){
                 if (eh_consumidor(u, sym)){
                     printf("----> Comparando com u: %s\n",agnameof(u));
-                    visita_vizinhanca(g, u);
+                    lista2 = visita_vizinhanca(g, u);
+                    interseccao(lista1, lista2);
+                    // diferenca(l1, l2);
+                    // diferenca(l2, l1);
+    
                 }
                 u = agnxtnode(g, u);
 
